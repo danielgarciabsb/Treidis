f621ca14b5500d9e319fe9d34741b95be4623d39
Daniel Garcia <danielgarciabsb@gmail.com>
2015-08-22 19:49:22 -0300

    Criado requisicoes, catch exceptions e logging

--- apirequests.py
+++ apirequests.py
@@ -34,17 +34,66 @@ class APIRequests(object):
     def __init__(self):
         pass
 
-    def requestAPI(self, url):
+    def __requestAPI(self, url, timeout):
         try:
-            data = requests.get(url, verify=False, timeout=0.001).json()
+            data = requests.get(url, verify=False, timeout=timeout)
+            data.raise_for_status()
+        except requests.exceptions.RequestException, e:
+            self.logger.error('RequestException: %s' % e)
+            raise
         except requests.exceptions.ConnectionError, e:
             self.logger.error('ConnectionError: %s' % e)
+            raise
+        except requests.exceptions.HTTPError, e:
+            self.logger.error('HTTPError: %s' % e)
+            raise
+        except requests.exceptions.URLRequired, e:
+            self.logger.error('URLRequired: %s' % e)
+            raise
+        except requests.exceptions.TooManyRedirects, e:
+            self.logger.error('TooManyRedirects: %s' % e)
+            raise
         except requests.exceptions.Timeout, e:
             self.logger.error('Timeout: %s' % e)
+            raise
+        else:
+            return data
+
+    def __makeRequest(self, api, timeout=5):
+        try:
+            data = self.__requestAPI(api, timeout)
+        except Exception, e:
+            self.logger.error('Falha ao obter reposta da API: %s' % api)
+        else:
+            self.logger.info('API obtida com sucesso: %s' % api)
+            return data
 
     def getFoxBitOrderbook(self):
-        data = self.requestAPI(self.FOXBIT_ORDERBOOK)
-        return data
+        return self.__makeRequest(self.FOXBIT_ORDERBOOK, 10)
+
+    def getFoxBitTrades(self):
+        return self.__makeRequest(self.FOXBIT_TRADES)
+
+    def getFoxBitTicker(self):
+        return self.__makeRequest(self.FOXBIT_TICKER)
+
+    def getBitfinexTicker(self):
+        return self.__makeRequest(self.BITFINEX_TICKER)
+
+    def getBitstampTicker(self):
+        return self.__makeRequest(self.BITSTAMP_TICKER)
+
+    def getOKCoinTicker(self):
+        return self.__makeRequest(self.OKCOIN_TICKER)
+
+    def getBTCeTicker(self):
+        return self.__makeRequest(self.BTCE_TICKER)
 
 fox = APIRequests()
-fox.getFoxBitOrderbook()
+print fox.getFoxBitOrderbook().json()
+print fox.getFoxBitTrades().json()
+print fox.getFoxBitTicker().json()
+print fox.getBitfinexTicker().json()
+print fox.getBitstampTicker().json()
+print fox.getOKCoinTicker().json()
+print fox.getBTCeTicker().json()
--- models.py
+++ models.py
@@ -1,1 +1,2 @@
 class Models(object):
+    pass
