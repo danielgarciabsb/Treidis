a33b984c3b0392009334335ac97a360e703ea89d
Daniel Garcia <danielgarciabsb@gmail.com>
2015-09-06 10:35:39 -0300

    Implementado spread e spread for amount em orderbook models

--- treidis/algoritmos/oportunista.py
+++ treidis/algoritmos/oportunista.py
@@ -1,5 +1,12 @@
+class Oportunista(object):
+
+    def __init__(self, investimento_maximo, taxa, api, log):
+        self.investimento_maximo = investimento_maximo
+        self.taxa = taxa
+
+    
 
 def oportunista(api, log):
-    log.info('Log do algoritmo oportunista')
-    print api.getBalance()
-    print 'oportunista finalizado'
+    log.info('Iniciando algoritmo Oportunista')
+    oportunista = Oportunista(100.0, 0.0025, api, log)
+    log.info('Algoritmo Oportunista finalizado')
